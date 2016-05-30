1017482d1a5a8228475f6d43f6bc6b553c2ed72c
Thomas Heslin <tjheslin1@gmail.com>
2016-05-30 15:17:14 +0100

    Working test for in memory mongo database. Need to fix maven running shell script.

---
+++ src/test/java/io/github/tjheslin1/learning/EmbeddedMongoTest.java
@@ -0,0 +1,78 @@
+package io.github.tjheslin1.learning;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.CreateCollectionOptions;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.IMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
+import io.github.tjheslin1.WithMockito;
+import io.github.tjheslin1.settings.Settings;
+import org.assertj.core.api.WithAssertions;
+import org.bson.Document;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+
+// http://mongodb.github.io/mongo-java-driver/2.13/getting-started/quick-tour/
+public class EmbeddedMongoTest implements WithAssertions, WithMockito {
+
+    private static final String DATABASE_NAME = "embedded";
+
+    private MongodExecutable mongodExe;
+    private MongodProcess mongod;
+    private MongoClient mongo;
+
+    private Settings settings = mock(Settings.class);
+
+    @Before
+    public void beforeEach() throws Exception {
+        when(settings.mongoDbPort()).thenReturn(27017);
+
+        MongodStarter mongodStarter = MongodStarter.getDefaultInstance();
+        IMongodConfig mongodConfig = new MongodConfigBuilder().version(Version.V3_2_1)
+                .net(new Net(settings.mongoDbPort(), Network.localhostIsIPv6())).build();
+        mongodExe = mongodStarter.prepare(mongodConfig);
+        mongod = mongodExe.start();
+        mongo = new MongoClient("localhost", settings.mongoDbPort());
+    }
+
+    @After
+    public void afterEach() throws Exception {
+        if (this.mongod != null) {
+            this.mongod.stop();
+            this.mongodExe.stop();
+        }
+    }
+
+    @Test
+    public void shouldCreateNewObjectInEmbeddedMongoDbv3() {
+        MongoDatabase db = mongo.getDatabase(DATABASE_NAME);
+        db.createCollection("testCollection", createCollectionOptions());
+
+        MongoCollection<Document> coll = db.getCollection("testCollection");
+
+        Document doc = new Document("restaurants", new Document("restaurant", "test"));
+        coll.insertOne(doc);
+
+        assertThat(db.getCollection("testCollection").count()).isEqualTo(1);
+    }
+
+    private CreateCollectionOptions createCollectionOptions() {
+        CreateCollectionOptions collectionOptions = new CreateCollectionOptions();
+        // options
+        return collectionOptions;
+
+    }
+}
