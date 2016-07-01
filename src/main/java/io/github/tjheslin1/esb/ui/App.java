bdc8233146ca16541a767e848d696e3adbd10f87
Thomas Heslin <tjheslin1@gmail.com>
2016-07-01 09:56:30 +0100

    Fixed commands and removed fields as they were being newed up in the wrong order.

--- src/main/java/io/github/tjheslin1/esb/ui/App.java
+++ src/main/java/io/github/tjheslin1/esb/ui/App.java
@@ -3,12 +3,11 @@ package io.github.tjheslin1.esb.ui;
 import com.mongodb.MongoClient;
 import io.github.tjheslin1.esb.application.BankAccountRetriever;
 import io.github.tjheslin1.esb.domain.BankAccount;
-import io.github.tjheslin1.esb.domain.cqrs.command.BalanceEventWriter;
-import io.github.tjheslin1.esb.domain.cqrs.query.BalanceEventReader;
 import io.github.tjheslin1.esb.domain.events.Balance;
 import io.github.tjheslin1.esb.infrastructure.domain.cqrs.command.MongoBalanceEventWriter;
 import io.github.tjheslin1.esb.infrastructure.domain.cqrs.query.MongoBalanceEventReader;
 import io.github.tjheslin1.esb.infrastructure.mongo.MongoConnection;
+import io.github.tjheslin1.esb.infrastructure.settings.MongoSettings;
 import io.github.tjheslin1.esb.infrastructure.settings.PropertiesReader;
 import io.github.tjheslin1.esb.infrastructure.settings.Settings;
 
@@ -25,25 +24,16 @@ public class App {
 
     public static final int ACCOUNT_ID = 7;
 
-    private static MongoConnection mongoConnection;
-    private static MongoClient mongoClient;
-    private static BalanceEventWriter eventWriter;
-    private static BalanceEventReader balanceEventReader;
-    private static BankAccountRetriever bankAccountRetriever;
-    private static Settings settings;
-
     public static void main(String[] args) {
-        settings = new Settings(new PropertiesReader("localhost"));
+        Settings settings = new Settings(new PropertiesReader("localhost"));
 
-        eventWriter = new MongoBalanceEventWriter(mongoClient, settings);
-        balanceEventReader = new MongoBalanceEventReader(mongoClient, settings);
+        MongoConnection mongoConnection = new MongoConnection(settings);
+        MongoClient mongoClient = mongoConnection.connection();
+        MongoBalanceEventWriter eventWriter = new MongoBalanceEventWriter(mongoClient, settings);
+        MongoBalanceEventReader balanceEventReader = new MongoBalanceEventReader(mongoClient, settings);
 
-        System.out.println("settings.mongoDbName() = " + settings.mongoDbName());
-
-        App app = new App();
-        app.loadTestData();
-
-        BankAccount testAccount = app.accountWithId(ACCOUNT_ID);
+        BankAccountRetriever bankAccountRetriever = new BankAccountRetriever();
+        BankAccount testAccount = bankAccountRetriever.bankAccountProjectionWithId(ACCOUNT_ID, balanceEventReader);
 
         printInstructions();
         Scanner scanner = new Scanner(System.in);
@@ -52,7 +42,9 @@ public class App {
         while (running) {
             String line = scanner.nextLine();
 
-            if (depositEvent(line)) {
+            if (exitCommand(line)) {
+                running = false;
+            } else if (depositEvent(line)) {
                 double amount = amountFromCommand(line);
                 eventWriter.write(depositFundsEvent(ACCOUNT_ID, amount, LocalDateTime.now()), depositEventWiring());
                 System.out.println(format("deposit event written for account: %s, for amount: %s.", ACCOUNT_ID, amount));
@@ -69,8 +61,6 @@ public class App {
                 bankAccountRetriever.sortedEvents(accountId, balanceEventReader, depositEventWiring(), withdrawalEventWiring())
                         .forEach(System.out::println);
                 System.out.println("Finished print events.");
-            } else if (exitCommand(line)) {
-                running = false;
             } else {
                 printInstructions();
             }
@@ -78,19 +68,6 @@ public class App {
         }
 
         System.out.println(format("Account with Id '%s' exists", testAccount.accountId()));
-    }
-
-    private void loadTestData() {
-        settings = new Settings(new PropertiesReader("localhost"));
-
-        mongoConnection = new MongoConnection(settings);
-        mongoClient = mongoConnection.connection();
-
-        bankAccountRetriever = new BankAccountRetriever();
-    }
-
-    private BankAccount accountWithId(int accountId) {
-        return bankAccountRetriever.bankAccountProjectionWithId(accountId, balanceEventReader);
     }
 
     private static void printInstructions() {
@@ -109,7 +86,7 @@ public class App {
     }
 
     private static boolean exitCommand(String line) {
-        return line.toLowerCase().toLowerCase().equals("exit");
+        return line.toLowerCase().equals("exit");
     }
 
     private static boolean depositEvent(String line) {
