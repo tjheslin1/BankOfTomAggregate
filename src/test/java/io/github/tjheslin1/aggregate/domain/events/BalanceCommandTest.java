45fc4570cbf7b65365ffafc244b52c2c37e3b9ca
Thomas Heslin <tjheslin1@gmail.com>
2016-08-14 21:09:28 +0100

    Added TODO.txt

--- src/test/java/io/github/tjheslin1/aggregate/domain/events/BalanceCommandTest.java
+++ src/test/java/io/github/tjheslin1/aggregate/domain/events/BalanceCommandTest.java
@@ -21,17 +21,16 @@ public class BalanceCommandTest implements WithAssertions {
     private final LocalDateTime timeOfFirstEvent = LocalDateTime.now(clock);
 
     private DepositFundsCommand firstDepositFundsCommand = depositFundsCommand(ACCOUNT_ID, 7, timeOfFirstEvent);
-    private DepositFundsCommand secondDepositFundsCommand = depositFundsCommand(ACCOUNT_ID, 9, timeOfFirstEvent.plusMinutes(5));
-    private DepositFundsCommand thirdDepositFundsCommand = depositFundsCommand(ANOTHER_ACCOUNT_ID, 9, timeOfFirstEvent.plusMinutes(5));
+    private DepositFundsCommand secondDepositFundsCommand = depositFundsCommand(ANOTHER_ACCOUNT_ID, 9, timeOfFirstEvent.plusMinutes(5));
 
     private WithdrawFundsCommand firstWithdrawFundsCommand = withdrawFundsCommand(ACCOUNT_ID, 4, timeOfFirstEvent.plusMinutes(10));
     private WithdrawFundsCommand secondWithdrawFundsCommand = withdrawFundsCommand(ANOTHER_ACCOUNT_ID, 4, timeOfFirstEvent);
 
     @Test
     public void compareToTest() {
-        assertThat(firstDepositFundsCommand.compareTo(thirdDepositFundsCommand)).isEqualTo(-1);
+        assertThat(firstDepositFundsCommand.compareTo(secondDepositFundsCommand)).isEqualTo(-1);
         assertThat(firstDepositFundsCommand.compareTo(firstWithdrawFundsCommand)).isEqualTo(-1);
-        assertThat(firstWithdrawFundsCommand.compareTo(thirdDepositFundsCommand)).isEqualTo(1);
+        assertThat(firstWithdrawFundsCommand.compareTo(secondDepositFundsCommand)).isEqualTo(1);
     }
 
     @Test(expected = IllegalStateException.class)
